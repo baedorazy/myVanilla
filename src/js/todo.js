b76c16864fb5acbb32fc9c512fe80448391ed0d8
baedorazy <jnbae@yellostory.co.kr>
2020-04-06 14:57:57 +0900

    Revert "Remove src"
    This reverts commit d8004f52

---
+++ src/js/todo.js
@@ -0,0 +1,75 @@
+(function() {
+    const todoForm = document.querySelector(".js-toDoForm"),
+        toodInput = todoForm.querySelector("input"),
+        todoUl = document.querySelector(".js-ul");
+    
+    const TODOS_LS = 'toDos';
+    let todos = [];
+    
+    function deleteTodo(event) {
+        const btn = event.target;
+        const li = btn.parentNode;
+        todoUl.removeChild(li);
+        
+        const cleanToDos = todos.filter(function(toDo) {
+            return toDo.id !== parseInt(li.id)
+        });
+        
+        todos = cleanToDos;
+        saveTodos();
+    }
+    
+    function saveTodos() {
+        localStorage.setItem(TODOS_LS, JSON.stringify(todos));
+    }
+    
+    function paintTodo(_todo) {
+        // setting
+        const li = document.createElement("li");
+        const deleteBtn = document.createElement("button");
+        const span = document.createElement("span");
+        const newId = todos.length + 1;
+        deleteBtn.innerText = "x";
+        deleteBtn.addEventListener("click", deleteTodo);
+        span.innerText = _todo;
+        li.id = newId;
+        li.appendChild(deleteBtn);
+        li.appendChild(span);
+        todoUl.append(li);
+        
+        const todoObj = {
+            text: _todo,
+            id: newId
+        };
+        
+        todos.push(todoObj);
+        saveTodos(todos);
+    }
+    
+    function todoHandleSubmit(e) {
+        e.preventDefault();
+        
+        const currentValue = toodInput.value;
+        paintTodo(currentValue);
+        toodInput.value = '';
+    }
+    
+    function loadTodos() {
+        const loadedToDos = localStorage.getItem(TODOS_LS);
+        
+        if(loadedToDos !== null) {
+            const parseTodos = JSON.parse(loadedToDos);
+            parseTodos.forEach(function(el) {
+                paintTodo(el.text)
+            })
+        } else {
+            console.log("first.");
+        }
+    }
+    
+    function init() {
+        loadTodos();
+        todoForm.addEventListener("submit", todoHandleSubmit);
+    }
+    init();
+})();
